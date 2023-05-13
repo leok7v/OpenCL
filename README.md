2b321425c6a9d4fb2018ae4d0a6dfb25a7ecb4cd
Leo Kuznetsov <leo.kuznetsov@gmail.com>
2023-05-13 00:11:45 -0700

    add.c simplified code for vectors addition and measured performance

--- README.md
+++ README.md
@@ -1,12 +1,18 @@
 # ocl - make OpenCL simple
 
-1. Updated project to Visual Studio 2022
-2. Do not want to instal Nvidia SDK or Intel SDK
-3. OpenCL header files are avaialbe at 
+[x] Updated project to Visual Studio 2022
+[x] Do not need to instal Nvidia SDK or Intel SDK
+[x] OpenCL header used from: 
    https://github.com/KhronosGroup/OpenCL-Headers/tree/main/CL
-4. OpenCL.dll exists on Windows and routes to both Intel and Nvidia drivers.
-5. We need to generate binding using GetProcAddress and trivial heared files parsing.
+[x] OpenCL.dll exists on Windows and routes to both Intel and Nvidia drivers.
+[x] Generated binding using GetProcAddress and trivial heared files parsing.
 
+# next step
+https://github.com/mattscar/opencl_dot_product
+
+# interesting kernels
+
+https://github.com/LibreScanner/opencv/blob/master/modules/ocl/src/opencl/kmeans_kernel.cl
 
 # original code was at
 https://github.com/pratikone/OpenCL-learning-VS2012
---
+++ add.c
@@ -0,0 +1,92 @@
+#include "rt.h"
+#include <CL/opencl.h>
+#include <assert.h>
+#include <windows.h>
+#include "ocl.h"
+
+enum { KB = 1024, MB = 1024 * KB, GB = 1024 * MB };
+
+enum { N = 1024 * 1024 };
+
+static cl_int x_add_y(ocl_context_t* c, ocl_command_queue_t q, ocl_kernel_t k,
+                      ocl_memory_t mx, ocl_memory_t my, ocl_memory_t mz) {
+    {   // initialize pinned memory:
+        float* x = ocl.map_write(q, mx, 0, N * sizeof(float));
+        float* y = ocl.map_write(q, my, 0, N * sizeof(float));
+        // two input vectors
+        for (int32_t i = 0; i < N; i++) { x[i] = (float)i; y[i] = (float)(N - i); }
+        ocl.unmap(q, mx, x);
+        ocl.unmap(q, my, y);
+    }
+    ocl_arg_t args[] =
+        {{mx, sizeof(ocl_memory_t)},
+         {my, sizeof(ocl_memory_t)},
+         {mz, sizeof(ocl_memory_t)}
+    };
+    ocl_event_t completion = ocl.enqueue_range_kernel(c, q, k, N, 0,
+        countof(args), args);
+    ocl.wait(&completion, 1);
+    ocl_profiling_t p = {0};
+    ocl.profile(completion, &p, N);
+    ocl.dispose_event(completion);
+    {   // map result and verify it:
+        float* z = (float*)ocl.map_read(q, mz, 0, N * sizeof(float));
+        for (int32_t i = 0; i < N; i++) {
+            float xi = (float)i;
+            float yi = (float)(N - i);
+            assertion(xi + yi == z[i], "%.1f + %.1f = %.1f\n", xi, yi, z[i]);
+        }
+        ocl.unmap(q, mz, z);
+    }
+    traceln("kernel execution on device: %.3f us (microseconds) Gops=%.3f",
+            p.time * (1000 * 1000), p.gops);
+    return 0;
+}
+
+#define kernel_name "x_add_y"
+
+static cl_int test(ocl_context_t* c) {
+    cl_int result = 0;
+    ocl_command_queue_t q = ocl.create_command_queue(c, true);
+    static const char* code =
+    "__kernel void " kernel_name "(__global const float* x, "
+    "                              __global const float* y, "
+    "                              __global float* z) {\n"
+    "    int i = get_global_id(0);\n"
+    "    z[i] = x[i] + y[i];\n"
+    "}\n";
+    ocl_program_t p = ocl.compile_program(c, code, strlen(code));
+    ocl_kernel_t k = ocl.create_kernel(p, kernel_name);
+    ocl_memory_t mx = ocl.allocate_write(c, N * sizeof(float));
+    ocl_memory_t my = ocl.allocate_write(c, N * sizeof(float));
+    ocl_memory_t mz = ocl.allocate_read(c, N * sizeof(float));
+    x_add_y(c, q, k, mx, my, mz);
+    x_add_y(c, q, k, mx, my, mz);
+    x_add_y(c, q, k, mx, my, mz);
+    ocl.flush_command_queue(q);   // must be called before deallocate()
+    ocl.finish_command_queue(q);  // must be called before deallocate()
+    ocl.deallocate(mx); // must be dealloca() before dispose_command_queue()
+    ocl.deallocate(my);
+    ocl.deallocate(mz);
+    ocl.dispose_kernel(k);
+    ocl.dispose_program(p);
+    ocl.dispose_command_queue(q);
+    return result;
+}
+
+int32_t main(int32_t argc, const char* argv[]) {
+    (void)argc; (void)argv;
+    cl_int result = 0;
+    ocl.init();
+    for (int cycles = 2; cycles > 0; cycles--) {
+        for (int i = 0; i < ocl.device_count; i++) {
+            ocl_context_t c = {0};
+            ocl.open(&c, i);
+            printf("%s\n", ocl.devices[i].name);
+            result = test(&c);
+            printf("test: %s\n", result == 0 ? "OK" : "FAILED");
+            ocl.close(&c);
+        }
+    }
+    return result;
+}
